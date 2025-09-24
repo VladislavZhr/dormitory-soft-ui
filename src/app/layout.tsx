95e52aa801cfa703fb4f8425b6fb542618ab5c8f
VladyslavZhr <strelkov.vlad2017@gmail.com>
2025-09-24 19:04:55 +0000

    refactor: delete HeroUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 
 import './globals.css';
-import Providers from '../shared/providers/Providers';
 
 export const metadata: Metadata = {
   title: 'example-hostel-front',
@@ -10,9 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="uk" className="light">
-      <body>
-        <Providers>{children}</Providers>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
